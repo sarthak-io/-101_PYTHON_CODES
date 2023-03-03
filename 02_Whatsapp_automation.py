88a71297ae2692b74fdbf7620e27bad6e71d2154
sarthak-io <sarthak25ic049@satiengg.in>
2023-03-03 23:49:26 +0530

    #Day2

--- 02_Whatsapp_automation.py
+++ 02_Whatsapp_automation.py
@@ -1,1 +1,11 @@
 #WHATSAPP AUTOMATION
+#pip install pywhatkit
+#import pywhatkit
+import pywhatkit
+#TAKE THESE INPUTS
+Mobile = input("ENTER RECEVIER MOBILE NO.(KINDLY USE +91):\n")
+Message =input("WRITE YOUR MESSAGE HERE\n")
+print("NOW ENTER SENDING TIME:")
+Hours=int(input("WRITE HOURS IN 24HOURS TIME CYCLE:\n"))
+Min=int(input("ENTER MINUTES:\n"))          
+pywhatkit.sendwhatmsg(Mobile,Message,Hours,Min,15)
